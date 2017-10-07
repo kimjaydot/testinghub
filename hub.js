e9072344f5b1a71ee34b646a61d70c6db01e4575
Kimberley Johnson <kimjaydot@gmail.com>
2017-10-07 06:43:56 -0500

    added name prompt

--- hub.js
+++ hub.js
@@ -1,4 +1,10 @@
+var name = prompt("What is your name", "Enter Name Here");
 var welcome = "Welcome to testing hub!";
 var number = 2 + 2 ;
 
+if (name !== "") {
+    alert("Please Enter Your name");
+}
+
+alert(welcome);
 alert(number);
