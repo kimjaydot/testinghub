eef867fe4dee5971ba5d70fea16fff5c39e67a2e
Kimberley Johnson <kimjaydot@gmail.com>
2017-10-07 06:49:22 -0500

    changed condition

--- hub.js
+++ hub.js
@@ -2,9 +2,9 @@ var name = prompt("What is your name", "Enter Name Here");
 var welcome = "Welcome to testing hub!";
 var number = 2 + 2 ;
 
-if (name !== "") {
+if (name !== "Kim") {
     alert("Please Enter Your name");
 }
 
-alert(welcome);
-alert(number);
+// alert(welcome);
+// alert(number);
