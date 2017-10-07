a4198f57524b311dc6592a89cbd8b12b3f77da9a
Kimberley Johnson <kimjaydot@gmail.com>
2017-10-07 06:55:20 -0500

    testing to see if alert box works

--- hub.js
+++ hub.js
@@ -1,14 +1,18 @@
 var name = prompt("What is your name", "Enter Name Here");
+var response = name;
 var welcome = "Welcome to testing hub!";
 var number = 2 + 2 ;
 
-if (name !== "Kim") {
-    alert("Please Enter Your name");
-}
 
-if (name === "Kim") {
-    alert("Welcome, " + number);
-}
+alert("Hi, " + response);
+
+// if (name !== "Kim") {
+//     alert("Please Enter Your name");
+// }
+
+// if (name === "Kim") {
+//     alert("Welcome, " + number);
+// }
 
 // alert(welcome);
 // alert(number);
