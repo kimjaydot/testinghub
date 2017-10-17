e49d2aab63aee4f26083acd40c583f5e3bd4dcdc
Kimberley Johnson <kimjaydot@gmail.com>
2017-10-17 16:54:08 -0500

    added push command

--- hub.js
+++ hub.js
@@ -2,6 +2,9 @@ var name = prompt("What is your name", "Enter Name Here");
 var response = name;
 var welcome = "Welcome to testing hub!";
 var number = 2 + 2 ;
+var cats = ["Smokey", "Kim", "Something"];
+cats.push("Sean", "Carey");
+console.log(cats);
 
 alert("Hello, " + response);
 
@@ -12,6 +15,14 @@ if (name !== "Kim") {
 if (name === "Smokey Johnson") {
     alert("Welcome, " + response);
 }
+
+
+// function add(param1, param2) {
+//     param1 = 1;
+//     param2 = 2; 
+//     var total = param1 + param2;
+// }
+// console.log(total);
 
 // alert(welcome);
 // alert(number);
